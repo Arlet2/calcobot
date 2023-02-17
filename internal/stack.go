1cedc8d4ae66b02902fcf07aba20fe0d76def607
Arlet2 <shulga@niuitmo.ru>
2023-02-18 01:45:17 +0300

    Add number stack

---
+++ internal/stack.go
@@ -0,0 +1,47 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+)
+
+type NumberStack interface {
+	Push(value float64)
+	Pop() float64
+}
+
+type NumberStackImpl struct {
+	array []float64
+}
+
+func (stack NumberStackImpl) Print() {
+	if len(stack.array) == 0 {
+		fmt.Println("EMPTY!")
+		return
+	}
+	for _, value := range stack.array {
+		fmt.Print(value, " <- ")
+	}
+	fmt.Println("bottom...")
+}
+
+func (stack *NumberStackImpl) Push(value float64)  {
+	newArray := make([]float64, 0)
+
+	newArray = append(newArray, value)
+	newArray = append(newArray, stack.array...)
+
+	stack.array = newArray
+
+}
+
+func (stack *NumberStackImpl) Pop() (float64, error) {
+	if len(stack.array) == 0 {
+		return 0, errors.New("stack is empty")
+	}
+	value := stack.array[0]
+
+	stack.array = stack.array[1:]
+
+	return value, nil
+}
