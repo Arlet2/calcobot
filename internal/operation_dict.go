f18a8924d1d0892036558cd14d71ff7dcdce3b64
Arlet2 <shulga@niuitmo.ru>
2023-02-19 01:06:02 +0300

    Split operation dict

---
+++ internal/operation_dict.go
@@ -0,0 +1,42 @@
+package internal
+
+import "errors"
+
+type OperationDict interface {
+	IsOnDict(symbol rune) bool
+	GetPriority(symbol rune) (int, error)
+	GetOperation(symbol rune) (operation, error)
+}
+
+type OperationDictImpl struct{
+	operations []operation
+}
+
+func (dict OperationDictImpl) IsOnDict(symbol rune) bool {
+	for _, value := range dict.operations {
+		if value.symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
+func (dict OperationDictImpl) GetPriority(symbol rune) (int, error) {
+	for _, value := range dict.operations {
+		if value.symbol == symbol {
+			return value.priority, nil
+		}
+	}
+
+	return 0, errors.New(string(symbol)+" operation is not allowed")
+}
+
+func (dict OperationDictImpl) GetOperation(symbol rune) (operation, error) {
+	for _, value := range dict.operations {
+		if value.symbol == symbol {
+			return value, nil
+		}
+	}
+
+	return operation{}, errors.New(string(symbol)+" operation is not allowed")
+}
