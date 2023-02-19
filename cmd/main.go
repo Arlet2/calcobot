efb3053d697c11b825f922f9137f7c089a1bb687
Arlet2 <shulga@niuitmo.ru>
2023-02-19 21:06:14 +0300

    Create main

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,26 @@
 package main
+
 import (
+	"calcobot/internal/database"
+	"calcobot/internal/network"
 	"fmt"
-	. "calcobot/internal"
+	"os"
 )
 
 func main() {
-	fmt.Println("Init")
+	db, err := database.NewPostgresDatabase(os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_NAME"), false)
 
-	
+	if err != nil {
+		fmt.Println("Found error: "+err.Error())
+		return
+	}
+
+	bot, err := network.NewBot(os.Getenv("BOT_TOKEN"), true)
+
+	if err != nil {
+		fmt.Println("Found error: "+err.Error())
+		return
+	}
+
+	bot.StartWorking(db)
 }
